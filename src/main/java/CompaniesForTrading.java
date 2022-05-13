95b7b8d80e9f948214a250484d6efbf50a68f8c2
pnevmv <isy02thisi@gmail.com>
2022-05-14 01:30:30 +0300

    getCompanies added

--- src/main/java/CompaniesForTrading.java
+++ src/main/java/CompaniesForTrading.java
@@ -3,12 +3,12 @@ import java.util.HashMap;
 public class CompaniesForTrading {
     HashMap<String, Company> companies;
 
-    public CompaniesForTrading() {
-
-    }
-
     public Company getByFigi(String figi) throws CompanyNotFoundException {
         if (!companies.containsKey(figi)) throw new CompanyNotFoundException();
         return companies.get(figi);
+    }
+
+    public HashMap<String, Company> getCompanies() {
+        return this.companies;
     }
 }
